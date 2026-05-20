c21f48ca160c26643acc8f3ba187f4c15d86f057
Shayak Banerjee <shayak.banerjee.13@gmail.com>
2026-05-20 11:30:06 +0100

    added module to download spy and gld data

---
+++ download_data.py
@@ -0,0 +1,28 @@
+from config import API_KEY
+from polygon import RESTClient
+import pandas as pd
+
+client = RESTClient(API_KEY)
+
+def get_data(ticker) -> pd.DataFrame:
+    aggs = []
+
+    for a in client.list_aggs(
+        ticker=ticker,
+        multiplier=1,
+        timespan='day',
+        from_='2026-01-01',
+        to='2026-05-01',
+        limit=50000
+    ):
+        aggs.append(a)
+
+    df = pd.DataFrame(aggs)
+
+    return df
+
+spy_df = get_data('SPY')
+spy_df.to_pickle("spy_data.pkl")
+
+gld_df = get_data("GLD")
+gld_df.to_pickle("gld_data.pkl")
